6d1b8696f7ca96455475e77692963e33b1d8e873
Lucy <lucy.chen.nz@gmail.com>
2018-09-20 21:51:07 +1200

    help im stuck on switching panes

--- src/Control/PracticeController.java
+++ src/Control/PracticeController.java
@@ -1,16 +1,23 @@
 package Control;
 
+import Model.Mediator;
 import Model.Practice;
 import Model.Practices;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Optional;
 
 /**
@@ -21,6 +28,12 @@ public class PracticeController {
 
     @FXML
     public Button playButton_3;
+
+    @FXML
+    public AnchorPane mainPane;
+
+    @FXML
+    public VBox rootVBox;
 
     /**
      * Plays the original .wav file when selecting a name, and pressing the play button
@@ -44,115 +57,25 @@ public class PracticeController {
     }
 
 
-
-    /**
-     * User prompted to record audio (generates creation which is added to the model)
-     * user is then asked to review the audio through another pop up.
-     */
-    public void recordAudioPopUp() {
-        Label l = new Label("Do you wish to record?");
-        l.setWrapText(true);
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Recording");
-        alert.setHeaderText("Recording");
-        alert.getDialogPane().setContent(l);
-
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK) {
-            Alert a = recordingDialog();
-
-            Task<Void> task = new Task<Void>() {
-                @Override
-                protected Void call() throws Exception {
-                    //generates file which adds to creation list
-                    String name = Practices.getInstance().getCurrentName();
-                    System.out.println(name);
-                    String fileName = Practices.getInstance().addNewPractice(name); //todo change addnewpractice return value
-                    System.out.println("don1e");
-                    Practices.getInstance().setFileName(fileName);
-                    System.out.println("done");
-                    return null;
-               }
-            };
-
-            //after creating the creation, user reviews the audio
-            task.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, e -> {
-                reviewAudioPopUp();
-                a.hide();
-            });
-
-            a.show();
-            Thread thread = new Thread(task);
-            thread.setDaemon(true);
-            thread.start();
-
-        } else {
-            alert.close();
-        }
-    }
-
-    public Alert recordingDialog() {
-        // show recording dialog
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Recording");
-        alert.setHeaderText("Recording dialog");
-        ButtonType dismissButton = new ButtonType("Ok", ButtonBar.ButtonData.CANCEL_CLOSE);
-        alert.getButtonTypes().setAll(dismissButton);
-
-        return alert;
-    }
-
-    public Alert reviewAudioPopUp() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION); //type of alert is a CONFIRMATION
-        alert.setTitle("Review"); //sets the title of alert
-        alert.setHeaderText("Review"); //sets the header
-        alert.setContentText("what would you like to do?");
-
-        ButtonType record = new ButtonType("Record", ButtonBar.ButtonData.OTHER);
-        ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
-        ButtonType listen = new ButtonType("Listen", ButtonBar.ButtonData.OTHER);
-        ButtonType done = new ButtonType("Done", ButtonBar.ButtonData.FINISH);
-
-        alert.getButtonTypes().setAll(done, listen, record, cancel);
-
-        Optional<ButtonType> result = alert.showAndWait(); //takes the selected button
-
-        if (result.get() == done) { //todo
-            //list_creations.getItems().add(name);
-            alert.close();
-        } else if (result.get() == listen) { //todo
-           // listenCreation(name);
-            alert.close();
-        } else if (result.get() == record) {
-            removeAudio();
-           recordAudioPopUp();
-            alert.close();
-
-        } else {
-            alert.close();
-        }
-
-        return alert;
-
-    }
-
     /**
      * when user clicks on practice button
      * @param event
      */
     public void practiceName(ActionEvent event){
-        recordAudioPopUp();
+        Mediator.getInstance().setPage("Page4");
+        switchTo("Main");
+
     }
 
-    /**
-     * gets rid of creation when user chooses to rerecord the audio.
-     */
-    public void removeAudio() {
-        String name = Practices.getInstance().getCurrentName();
-        String fileName = Practices.getInstance().getFileName();
-        System.out.println(name);
-        System.out.println("file to delete"+ fileName);
-        Practices.getInstance().deletePractice(name, fileName);
+    private void switchTo(String scene) {
+        try {
+            URL url = new File("src/GUI/" + scene + ".fxml").toURL();
+            VBox root = FXMLLoader.load(url);
+            rootVBox.getChildren().setAll(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
---
+++ src/Control/RecordController.java
@@ -0,0 +1,123 @@
+package Control;
+
+import Model.Practices;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import java.util.Optional;
+
+public class RecordController {
+
+    @FXML
+    public Button recordButton_4;
+
+    @FXML
+    public VBox rootVBox;
+
+    /**
+     * User prompted to record audio (generates creation which is added to the model)
+     * user is then asked to review the audio through another pop up.
+     */
+    public void recordAudioPopUp() {
+            Alert a = recordingDialog();
+
+            Task<Void> task = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    //generates file which adds to creation list
+                    String name = Practices.getInstance().getCurrentName();
+                    System.out.println(name);
+                    String fileName = Practices.getInstance().addNewPractice(name); //todo change addnewpractice return value
+                    System.out.println("don1e");
+                    Practices.getInstance().setFileName(fileName);
+                    System.out.println("done");
+                    return null;
+                }
+            };
+
+            //after creating the creation, user reviews the audio
+            task.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, e -> {
+                reviewAudioPopUp();
+                a.hide();
+            });
+
+            a.show();
+            Thread thread = new Thread(task);
+            thread.setDaemon(true);
+            thread.start();
+            
+    }
+
+    public void record(ActionEvent event){
+        recordAudioPopUp();
+    }
+
+    public Alert recordingDialog() {
+        // show recording dialog
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Recording");
+        alert.setHeaderText("Recording dialog");
+        ButtonType dismissButton = new ButtonType("Ok", ButtonBar.ButtonData.CANCEL_CLOSE);
+        alert.getButtonTypes().setAll(dismissButton);
+
+        return alert;
+    }
+
+    public Alert reviewAudioPopUp() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION); //type of alert is a CONFIRMATION
+        alert.setTitle("Review"); //sets the title of alert
+        alert.setHeaderText("Review"); //sets the header
+        alert.setContentText("what would you like to do?");
+
+        ButtonType record = new ButtonType("Record", ButtonBar.ButtonData.OTHER);
+        ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+        ButtonType listen = new ButtonType("Listen", ButtonBar.ButtonData.OTHER);
+        ButtonType done = new ButtonType("Done", ButtonBar.ButtonData.FINISH);
+
+        alert.getButtonTypes().setAll(done, listen, record, cancel);
+
+        Optional<ButtonType> result = alert.showAndWait(); //takes the selected button
+
+        if (result.get() == done) { //todo
+            //list_creations.getItems().add(name);
+            alert.close();
+        } else if (result.get() == listen) { //todo
+            // listenCreation(name);
+            alert.close();
+        } else if (result.get() == record) {
+            removeAudio();
+            recordAudioPopUp();
+            alert.close();
+
+        } else {
+            alert.close();
+        }
+
+        return alert;
+
+    }
+
+    /**
+     * when user clicks on practice button
+     * @param event
+     */
+    public void practiceName(ActionEvent event){
+        recordAudioPopUp();
+    }
+
+    /**
+     * gets rid of creation when user chooses to rerecord the audio.
+     */
+    public void removeAudio() {
+        String name = Practices.getInstance().getCurrentName();
+        String fileName = Practices.getInstance().getFileName();
+        System.out.println(name);
+        System.out.println("file to delete"+ fileName);
+        Practices.getInstance().deletePractice(name, fileName);
+    }
+
+}
