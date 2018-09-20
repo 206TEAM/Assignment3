ba83c80a928e4384eafa9b4cffac30c99e5cb02f
Eric Pedrido <epedrids@gmail.com>
2018-09-21 02:20:19 +1200

    Slider changed to working progress bar.

--- src/Control/PracticeController.java
+++ src/Control/PracticeController.java
@@ -4,14 +4,27 @@ import Model.Mediator;
 import Model.Original;
 import Model.Originals;
 import Model.Practices;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * This class controls Page3 where the user can choose to play and/or practice a name
@@ -23,7 +36,7 @@ public class PracticeController {
 
 	@FXML public Button playButton_3;
 	@FXML public Button practiceButton_3;
-	@FXML public Slider originalSlider_3;
+	@FXML public ProgressBar progressBar;
 
 	/**
 	 * Plays the original .wav file when selecting a name, and pressing the play button
@@ -31,9 +44,15 @@ public class PracticeController {
 	public void playOriginal(ActionEvent event) {
 		System.out.println("played");
 		String name = Practices.getInstance().getCurrentName();
-		Originals.getInstance().playOriginal(name);
+		Thread thread = new Thread(() -> {
+			Originals.getInstance().playOriginal(name);
+		});
+		thread.setDaemon(true);
+		thread.start();
+		showProgress();
 //        MediaPlayer mediaPlayer = new MediaPlayer(createMedia(name));
 //        mediaPlayer.play();
+
 	}
 
 	/**
@@ -47,7 +66,6 @@ public class PracticeController {
 		return media;
 	}
 
-
 	/**
 	 * when user clicks on practice button
 	 *
@@ -56,5 +74,28 @@ public class PracticeController {
 	public void practiceName(ActionEvent event) {
 		Mediator.getInstance().setPage("Page4");
 		Mediator.getInstance().loadPane();
+	}
+
+	private void showProgress() {
+		double duration = 0;
+		try {
+			String name = Practices.getInstance().getCurrentName();
+			String fileName = Originals.getInstance().getFileName(name).get(0);
+			File file = new File("Names/" + name + "/Original/" + fileName);
+			AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+			AudioFormat format = ais.getFormat();
+
+			long frames = ais.getFrameLength();
+			duration = (frames+0.0) / format.getFrameRate();
+		} catch (UnsupportedAudioFileException | IOException e) {
+			e.printStackTrace();
+		}
+
+		Timeline timeLine = new Timeline(
+				new KeyFrame(Duration.ZERO, new KeyValue(progressBar.progressProperty(), 0)),
+				new KeyFrame(Duration.seconds(duration), Event::consume,
+						new KeyValue(progressBar.progressProperty(), 1)));
+		timeLine.setCycleCount(1);
+		timeLine.play();
 	}
 }
